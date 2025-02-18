49402c1e32fd3e32b64908f6f72a919f7df5f9ac
Alexis <90972106+alexishadjian@users.noreply.github.com>
2025-02-18 15:21:20 +0100

    feat: add homepage news block condition

--- assets/rubis/plugins/dna-rubis/homepage.php
+++ assets/rubis/plugins/dna-rubis/homepage.php
@@ -60,45 +60,49 @@
 
 <?php endif; ?>
 
-<?php if ($fields_hp["block_actus"]): ?>
-    <section class="actusHp">
-        <div class="container">
-            <div class="row title">
-                <div class="col-lg-6 col-md-6">
-                    <h2><?php esc_html_e($fields_hp["block_actus"]["title"]); ?></h2>
-                </div>
-                <div class="col-lg-6 col-md-6 col-cta">
-                    <a class="cta green" target="<?php esc_html_e($fields_hp["block_actus"]["link"]["target"]); ?>" href="<?php esc_html_e($fields_hp["block_actus"]["link"]["url"]); ?>"><?php esc_html_e($fields_hp["block_actus"]["link"]["title"]); ?></a>
-                </div>
-            </div>
+<?php if ($fields_hp["is_block_actus"]): ?>
 
-            <?php $actus = get_last_actus(); ?>
-            <?php if ($actus): ?>
+    <?php if ($fields_hp["block_actus"]): ?>
+        <section class="actusHp">
+            <div class="container">
+                <div class="row title">
+                    <div class="col-lg-6 col-md-6">
+                        <h2><?php esc_html_e($fields_hp["block_actus"]["title"]); ?></h2>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-cta">
+                        <a class="cta green" target="<?php esc_html_e($fields_hp["block_actus"]["link"]["target"]); ?>" href="<?php esc_html_e($fields_hp["block_actus"]["link"]["url"]); ?>"><?php esc_html_e($fields_hp["block_actus"]["link"]["title"]); ?></a>
+                    </div>
+                </div>
+
+                <?php $actus = get_last_actus(); ?>
+                <?php if ($actus): ?>
+                    
+                    <div class="row">
+                        <?php foreach ($actus as $actu): ?>
+                            <div class="col-lg-4 col-eachCardActu">
+                                <a href="<?php esc_html_e(get_the_permalink($actu->ID)); ?>">
+                                    <div class="containerImg">
+                                        <div class="img" style="background-image: url('<?php esc_html_e(get_the_post_thumbnail_url($actu->ID)); ?>')"></div> 
+                                    </div>                                   
+                                    <?php $date = get_the_date('Y-m-d', $actu->ID); ?>
+                                    <div class="date"><?php echo date_i18n('d F Y', strtotime($date)); ?></div>
+
+                                    <h3><?php esc_html_e(get_the_title($actu->ID)); ?></h3>
+
+                                    <div class="content"><?php echo apply_filters('the_content', $actu->post_content) ?></div>
+
+                                    <div class="hypertext"><?php pll_e('Lire la suite') ?></div>
+                                </a>
+                            </div>
+
+                        <?php endforeach; ?>
+                    </div>
                 
-                <div class="row">
-                    <?php foreach ($actus as $actu): ?>
-                        <div class="col-lg-4 col-eachCardActu">
-                            <a href="<?php esc_html_e(get_the_permalink($actu->ID)); ?>">
-                                <div class="containerImg">
-                                    <div class="img" style="background-image: url('<?php esc_html_e(get_the_post_thumbnail_url($actu->ID)); ?>')"></div> 
-                                </div>                                   
-                                <?php $date = get_the_date('Y-m-d', $actu->ID); ?>
-                                <div class="date"><?php echo date_i18n('d F Y', strtotime($date)); ?></div>
+                <?php endif; ?>
+            </div>
+        </section>
 
-                                <h3><?php esc_html_e(get_the_title($actu->ID)); ?></h3>
-
-                                <div class="content"><?php echo apply_filters('the_content', $actu->post_content) ?></div>
-
-                                <div class="hypertext"><?php pll_e('Lire la suite') ?></div>
-                            </a>
-                        </div>
-
-                    <?php endforeach; ?>
-                </div>
-            
-            <?php endif; ?>
-        </div>
-    </section>
+    <?php endif; ?>
 
 <?php endif; ?>
 
