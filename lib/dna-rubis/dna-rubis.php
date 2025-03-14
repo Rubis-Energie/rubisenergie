7a910bd69ee7bd55f3ddf816b4f94ae77a78dff4
Alexis <90972106+alexishadjian@users.noreply.github.com>
2025-03-14 12:35:27 +0100

    refact: move implantation CPT to main plugin

--- lib/dna-rubis/dna-rubis.php
+++ lib/dna-rubis/dna-rubis.php
@@ -154,6 +154,7 @@ $dna_theme_load = new Dna_Rubis_Load();
 
 // Load Custom Post Type
 require_once(DNA_RUBIS_PLUGIN_DIR.'/cpt/cpt-product.php');
+require_once(DNA_RUBIS_PLUGIN_DIR.'/cpt/cpt-implantation.php');
 
 
 
