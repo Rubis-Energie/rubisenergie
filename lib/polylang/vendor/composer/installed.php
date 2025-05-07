32a4176bd54747a665be9d0dabd6ecb94dbd690e
Thomas <hello@thomasflorentin.net>
2025-05-07 11:30:46 +0200

    updates: Polylang

--- lib/polylang/vendor/composer/installed.php
+++ lib/polylang/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'wpsyntex/polylang',
-        'pretty_version' => '3.6.x-dev',
-        'version' => '3.6.9999999.9999999-dev',
-        'reference' => 'f89781f84bf98b991f68e23c4f9eef800fa730a7',
+        'pretty_version' => '3.7.x-dev',
+        'version' => '3.7.9999999.9999999-dev',
+        'reference' => 'c9cc69a8d359f38742a26cd353428f7d9d4f3d92',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'wpsyntex/polylang' => array(
-            'pretty_version' => '3.6.x-dev',
-            'version' => '3.6.9999999.9999999-dev',
-            'reference' => 'f89781f84bf98b991f68e23c4f9eef800fa730a7',
+            'pretty_version' => '3.7.x-dev',
+            'version' => '3.7.9999999.9999999-dev',
+            'reference' => 'c9cc69a8d359f38742a26cd353428f7d9d4f3d92',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
