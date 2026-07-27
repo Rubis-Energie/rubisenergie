70bb1ef8953c6cfa3f9f21310d462c913013d508
Thomas <hello@thomasflorentin.net>
2026-07-27 09:05:28 +0200

    updates: Redirection

--- lib/redirection/build/redirection.asset.php
+++ lib/redirection/build/redirection.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-element', 'wp-i18n'), 'version' => '1e46303089c718887d2a');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-element', 'wp-i18n'), 'version' => 'ac9f75394c284b299fa7');
